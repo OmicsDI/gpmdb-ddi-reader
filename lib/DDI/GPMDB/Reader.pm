b559c9ec7f32de3221eb899b797e4b88fe969952
Yasset Perez-Riverol <ypriverol@gmail.com>
2015-11-18 15:48:03 +0000

    Update Reader.pm

--- lib/DDI/GPMDB/Reader.pm
+++ lib/DDI/GPMDB/Reader.pm
@@ -29,7 +29,7 @@ sub create_reference_files {
 	say "Processing directory $dir...";
 
   # instantiate parallel processing module
-  my $pm = Parallel::ForkManager->new($self->{procs}, '/home/felipevl/Workspace/DDI-GPMDB-Reader/data/temp');
+  my $pm = Parallel::ForkManager->new($self->{procs}, './data/temp/');
   my %responses = ();
   my @responses;
 
